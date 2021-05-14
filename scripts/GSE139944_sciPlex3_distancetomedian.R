b94d03f2ab01f4d9bede561a836da75668af91d9
Xin Wen Yap <yapxinwen1608@gmail.com>
2021-05-14 16:32:29 -0400

    add distance to median analysis

--- scripts/GSE139944_sciPlex3_distancetomedian.R
+++ scripts/GSE139944_sciPlex3_distancetomedian.R
@@ -5,8 +5,31 @@ library(scran)
 library(monocle3)
 library(dplyr)
 library(ggplot2)
+library(Hmisc)
+library(gdata)
+library(vioplot)
+library(glue)
 
 #### Functions ####
+dm_analysis <- function(counts, top.n) {
+  means <- rowMeans(counts)
+  cv2 <- apply(counts, 1, var) / (means ^ 2)
+  
+  dm <- DM(means, cv2)
+  dm <- dm[!(is.na(dm))]
+  dm <- dm[order(abs(dm), decreasing=TRUE)]
+  
+  return(head(dm, top.n))
+}
+
+correlation_analysis <- function(counts, top.n.dm) {
+  return(rcorr(t(as.matrix(counts[names(top.n.dm), ])),
+               type="spearman"))
+}
+
+dcorr <- function(corr) {
+  sqrt((1 - upperTriangle(corr$r)) / 2)
+}
 
 #### Config ####
 # Set working directory
@@ -48,26 +71,71 @@ filt.gene.data <- fData(readRDS("./processed/GSE139944/GSM4150378_sciPlex3_cds_a
 rownames(filt.counts) <- filt.gene.data$gene_short_name
 rownames(filt.gene.data) <- filt.gene.data$gene_short_name
 
-#### DM analysis ####
+#### DM and correlation analysis ####
+dm.corr.list <- list()
+
 # Parameters
 cell.type <- "K562"
 product <- "Luminespib"
-d <- 10000
+d <- c(10, 100, 1000, 10000)
 
-sample.cells <- filt.col.data[grep(product, filt.col.data$product_name), ] %>%
-  filter(cell_type == cell.type, dose == d) %>% 
+top.n <- 500
+
+vehicle.cells <- filt.col.data[filt.col.data$vehicle, ] %>%
+  filter(cell_type == cell.type) %>%
   rownames()
+vehicle.counts <- filt.counts[, vehicle.cells]
 
-sample.counts <- filt.counts[, sample.cells]
+# RPM normalisation
+vehicle.rpm <- vehicle.counts / (colSums(vehicle.counts) / 10^6)
 
-# Normalisation by RPM
-sample.rpm <- sample.counts / (colSums(sample.counts) / 10^6)
+# Exclude genes with low mean expression
+vehicle.rpm <- vehicle.rpm[rowMeans(vehicle.rpm) > 10, ]
 
-# Distance to median metric
-means.sample <- rowMeans(sample.rpm)
+# Get distance-to-median metric
+dm.corr.list[["Vehicle_0_topdm"]] <- dm_analysis(vehicle.rpm, top.n)
 
-cv2.sample <- apply(sample.rpm, 1, var) / (means.sample ^ 2)
+# Get correlation metric
+dm.corr.list[["Vehicle_0_corr"]] <- correlation_analysis(vehicle.rpm,
+                                                         dm.corr.list[["Vehicle_0_topdm"]])
 
-dm.stat.sample <- DM(means.sample, cv2.sample)
-dm.stat.sample <- dm.stat.sample[!(is.na(dm.stat.sample))]
-dm.stat.sample <- dm.stat.sample[order(abs(dm.stat.sample), decreasing=TRUE)]
+# Repeat for product
+for (i in 1:length(d)) {
+  sample.cells <- filt.col.data[grep(product, filt.col.data$product_name), ] %>%
+    filter(cell_type == cell.type, dose == d[i]) %>% 
+    rownames()
+  sample.counts <- filt.counts[, sample.cells]
+  
+  # RPM normalisation
+  sample.rpm <- sample.counts / (colSums(sample.counts) / 10^6)
+  
+  # Exclude genes with low mean expression
+  sample.rpm <- sample.rpm[rowMeans(sample.rpm) > 10, ]
+  
+  # Get distance-to-median metric
+  dm.corr.list[[paste0(product, "_", d[i], "_topdm")]] <- dm_analysis(sample.rpm, top.n)
+  
+  # Get correlation metric
+  dm.corr.list[[paste0(product, "_", d[i], "_corr")]] <- correlation_analysis(sample.rpm,
+                                                                              dm.corr.list[[paste0(product, "_", d[i], "_topdm")]])
+}
+
+# Plot violin plot
+png(glue("./processed/GSE139944/transcriptional_noise/sciPlex3_DM_{cell.type}_{product}.png"),
+    width=3000, height=2000, res=300)
+vioplot(dcorr(dm.corr.list[["Vehicle_0_corr"]]), 
+        dcorr(dm.corr.list[[paste0(product, "_10_corr")]]),
+        dcorr(dm.corr.list[[paste0(product, "_100_corr")]]),
+        dcorr(dm.corr.list[[paste0(product, "_1000_corr")]]),
+        dcorr(dm.corr.list[[paste0(product, "_10000_corr")]]),
+        names=c("Vehicle_0", 
+                paste0(product, "_10"),
+                paste0(product, "_100"),
+                paste0(product, "_1000"),
+                paste0(product, "_10000")), 
+        col="orange")
+title(main=glue("{cell.type} transcriptional heterogeneity in {product} treatments
+                for the top N={top.n} highly variable genes"),
+      ylab="Pairwise distance measure")
+dev.off()
+
